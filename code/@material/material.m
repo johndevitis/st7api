f2c6e607c15bc1ce5d71121ac7c9cc863e60c9b6
jbbraley <jbbraley@gmail.com>
2016-09-07 15:12:03 -0400

    modified material class

--- code/@material/material.m
+++ code/@material/material.m
@@ -1,6 +1,43 @@
-classdef material < handle
+classdef material < matlab.mixin.SetGet
+%% classdef material
+% 
+% 
+% 
+% author: John Braley
+% create date: 29-Aug-2016 15:44:09
 
+%% object properties
+	properties
+        name
+        density
+        modulus
+        thermal
+	end
 
+%% dependent properties
+	properties (Dependent)
+	end
+
+%% private properties
+	properties (Access = private)
+	end
+
+%% dynamic methods
+	methods
+	%% constructor
+		function self = material()
+		end
+
+	%% dependent methods
+
+	end
+
+%% static methods
+	methods (Static)
+	end
+
+%% protected methods
+	methods (Access = protected)
+	end
 
 end
-
